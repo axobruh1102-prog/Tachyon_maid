d7b79acdc68e6dd0b4a84916ed05b4505822f102
jst_axo <axobruh1102@gmail.com>
2026-09-25 00:08:36 +0700

    Add slash command registration for '/maid'
    Register a new slash command '/maid' for the Discord bot.

--- server.js
+++ server.js
@@ -33,4 +33,24 @@ client.on("messageCreate", (message) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
+const commands = [
+  new SlashCommandBuilder()
+    .setName("maid")
+    .setDescription("Gọi Maid Tachyon")
+].map(command => command.toJSON());
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("Slash command /maid registered!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
