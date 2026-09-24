938f389255aec85cac165bb5585a983c02cdc97a
jst_axo <axobruh1102@gmail.com>
2026-09-24 23:34:37 +0700

    Refactor server.js for improved logging and structure

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
 const express = require("express");
-
-const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("🧋 Maid Tachyon is alive!");
-});
-
-app.post("/discord/events", (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
+// Web server
 app.get("/", (req, res) => {
   res.send("🗿 Maid Tachyon is alive!");
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(Server running on port ${PORT});
+  console.log(🌐 Server running on port ${PORT});
 });
 
-// Discord Bot
+// Discord bot
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -43,7 +23,7 @@ const client = new Client({
 });
 
 client.once("ready", () => {
-  console.log(🤖 Logged in as ${client.user.tag});
+  console.log(🤖 ${client.user.tag} is online!);
 });
 
 client.login(process.env.DISCORD_TOKEN);
