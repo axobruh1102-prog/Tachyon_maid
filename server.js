98e06a2aff0b21ce25baadba281e381fc7b3d3c4
jst_axo <axobruh1102@gmail.com>
2026-09-24 23:32:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,3 +17,33 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🗿 Maid Tachyon is alive!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(Server running on port ${PORT});
+});
+
+// Discord Bot
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(🤖 Logged in as ${client.user.tag});
+});
+
+client.login(process.env.DISCORD_TOKEN);
